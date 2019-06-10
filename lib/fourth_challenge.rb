28357ec2fcae7d0ccab1c6ff15455b8755c253e9
Kelliann Lafferty <kell10192@gmail.com>
2019-06-10 22:15:54 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -14,9 +14,9 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
-          {name: "Nurse", age: "44", attitude: "worried"}
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
-  }
+}
 end
